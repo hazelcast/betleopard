fe57c6d76a79dbe80557717c584aebe9eb424778
Ben Evans <benjamin.john.evans@gmail.com>
2016-11-12 01:45:03 +0000

    EOD

--- src/main/java/com/betleopard/hazelcast/LiveBetMain.java
+++ src/main/java/com/betleopard/hazelcast/LiveBetMain.java
@@ -7,9 +7,7 @@ import com.betleopard.domain.Horse;
 import com.betleopard.domain.Race;
 import com.hazelcast.client.HazelcastClient;
 import com.hazelcast.core.HazelcastInstance;
-import com.hazelcast.core.IList;
 import com.hazelcast.core.IMap;
-import com.hazelcast.core.ISet;
 import com.hazelcast.spark.connector.rdd.HazelcastRDDFunctions;
 import static com.hazelcast.spark.connector.HazelcastJavaPairRDDFunctions.javaPairRddFunctions;
 import java.time.DayOfWeek;
@@ -17,10 +15,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.temporal.TemporalAdjusters;
-import java.util.Map;
-import java.util.Set;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.*;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
@@ -35,6 +30,7 @@ public class LiveBetMain {
 
     private JavaSparkContext sc;
     private volatile boolean shutdown = false;
+    private final HazelcastInstance client = HazelcastClient.newHazelcastClient();
 
     public static void main(String[] args) {
         final HazelcastFactory<Horse> stable = HazelcastHorseFactory.getInstance();
@@ -72,7 +68,7 @@ public class LiveBetMain {
         final HazelcastRDDFunctions accessToHC = javaPairRddFunctions(winners);
         accessToHC.saveToHazelcastMap("winners");
 
-        createFutureEvents();
+        createFutureEvent();
     }
 
     public void stop() {
@@ -99,40 +95,63 @@ public class LiveBetMain {
         }
     }
 
-    public void createFutureEvents() {
+    public void createFutureEvent() {
         // Grab some horses to use as runners in races
-        final HazelcastInstance client = HazelcastClient.newHazelcastClient();
         final IMap<Horse, Object> fromHC = client.getMap("winners");
-        
         final Set<Horse> horses = fromHC.keySet();
-        final ISet<Event> events = client.getSet("saturday");
-        
+
         // Now set up some future-dated events for next Sat
         final LocalDate nextSat = LocalDate.now().with(TemporalAdjusters.next(DayOfWeek.SATURDAY));
         LocalTime raceTime = LocalTime.of(11, 0); // 1100 start
         final Event e = CentralFactory.eventOf("Racing from Epsom", nextSat);
-        final Set<Horse> runners = makeRunners(horses);
-        for (int i=0; i<18; i++) {
+        final Set<Horse> runners = makeRunners(horses, 10);
+        for (int i = 0; i < 18; i++) {
             final Map<Horse, Double> runnersWithOdds = makeSimulatedOdds(runners);
-            CentralFactory.raceOf(LocalDateTime.of(nextSat, raceTime), runnersWithOdds);
-            
+            final Race r = CentralFactory.raceOf(LocalDateTime.of(nextSat, raceTime), runnersWithOdds);
+            e.addRace(r);
+
             raceTime = raceTime.plusMinutes(10);
         }
+        final IMap<Long, Event> events = client.getMap("events");
+        events.put(e.getID(), e);
     }
 
     public void addSomeSimulatedBets() {
-
+        final IMap<Long, Event> events = client.getMap("events");
+        final int numBets = 100;
+        for (int i=0; i<numBets; i++) {
+            final Race r = getRandomRace(events);
+            r.getCurrentVersion().getOdds();
+        }
     }
 
     public void recalculateRiskReports() {
 
     }
 
-    private Set<Horse> makeRunners(Set<Horse> horses) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    Set<Horse> makeRunners(final Set<Horse> horses, int num) {
+        if (horses.size() < num) {
+            return horses;
+        }
+        final Set<Horse> out = new HashSet<>();
+        final Iterator<Horse> it = horses.iterator();
+        for (int i = 0; i < num; i++) {
+            out.add(it.next());
+        }
+        return out;
     }
 
-    private Map<Horse, Double> makeSimulatedOdds(Set<Horse> runners) {
+    Map<Horse, Double> makeSimulatedOdds(final Set<Horse> runners) {
+        final Set<Horse> thisRace = makeRunners(runners, 4);
+        final Map<Horse, Double> out = new HashMap<>();
+        int i = 1;
+        for (Horse h : thisRace) {
+            out.put(h, Math.random() * i++);
+        }
+        return out;
+    }
+
+    private Race getRandomRace(final IMap<Long, Event> events) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
