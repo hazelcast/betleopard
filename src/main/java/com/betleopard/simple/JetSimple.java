0e09053845e8992bfd61e6c8059d796ba6a9aa32
Ben Evans <benjamin.john.evans@gmail.com>
2017-05-24 07:35:57 +0200

    Some progress

--- src/main/java/com/betleopard/simple/JetSimple.java
+++ src/main/java/com/betleopard/simple/JetSimple.java
@@ -4,9 +4,6 @@ import com.betleopard.JSONSerializable;
 import com.betleopard.domain.CentralFactory;
 import com.betleopard.domain.Event;
 import com.betleopard.domain.Horse;
-import static com.betleopard.hazelcast.JetBetMain.USER_ID;
-import static com.betleopard.hazelcast.JetBetMain.WORST_ID;
-import com.hazelcast.client.HazelcastClient;
 import com.hazelcast.client.config.ClientConfig;
 import com.hazelcast.core.IMap;
 import com.hazelcast.jet.DAG;
@@ -28,6 +25,9 @@ import java.util.*;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import static com.hazelcast.jet.Processors.map;
+import static com.hazelcast.jet.Util.entry;
+import java.util.Map.Entry;
 
 /**
  * Simple example for getting started - uses Jet adapted from Java 8 
@@ -38,6 +38,13 @@ import java.util.stream.Collectors;
 public class JetSimple {
 
     public final static String EVENTS_BY_NAME = "events_by_name";
+    public final static String MULTIPLE = "multiple_winners";
+
+    private final static String HISTORICAL = "historical_races.json";
+
+    private final static Function<Event, Horse> FIRST_PAST_THE_POST = e -> e.getRaces().get(0).getWinner().orElse(Horse.PALE);
+    private final static Function<Map.Entry<Horse, ?>, Horse> UNDER_1 = entry -> entry.getKey();
+    private final static Function<Map.Entry<Horse, Integer>, Integer> UNDER_2 = entry -> entry.getValue();
 
     private JetInstance jet;
 
@@ -57,10 +64,9 @@ public class JetSimple {
                 .map(s -> JSONSerializable.parse(s, Event::parseBag))
                 .collect(Collectors.toList());
 
-        final Function<Event, Horse> fptp = e -> e.getRaces().get(0).getWinner().orElse(Horse.PALE);
         final Map<Event, Horse> winners
                 = events.stream()
-                .collect(Collectors.toMap(Function.identity(), fptp));
+                .collect(Collectors.toMap(Function.identity(), FIRST_PAST_THE_POST));
 
         final Map<Horse, Set<Event>> inverted = new HashMap<>();
         for (Map.Entry<Event, Horse> entry : winners.entrySet()) {
@@ -70,20 +76,33 @@ public class JetSimple {
             inverted.get(entry.getValue()).add(entry.getKey());
         }
 
-        final Function<Map.Entry<Horse, ?>, Horse> under1 = entry -> entry.getKey();
-        final Function<Map.Entry<Horse, Integer>, Integer> under2 = entry -> entry.getValue();
         final Function<Map.Entry<Horse, Set<Event>>, Integer> setCount = entry -> entry.getValue().size();
         final Map<Horse, Integer> withWinCount
                 = inverted.entrySet().stream()
-                .collect(Collectors.toMap(under1, setCount));
+                .collect(Collectors.toMap(UNDER_1, setCount));
 
         final Map<Horse, Integer> multipleWinners
                 = withWinCount.entrySet().stream()
                 .filter(entry -> entry.getValue() > 1)
-                .collect(Collectors.toMap(under1, under2));
+                .collect(Collectors.toMap(UNDER_1, UNDER_2));
 
         System.out.println("Multiple Winners from List :");
         System.out.println(multipleWinners);
+    }
+
+    public static DAG buildDag() {
+        final DAG dag = new DAG();
+
+        final Vertex source = dag.newVertex("source", readMap(EVENTS_BY_NAME));
+        final Vertex winners = dag.newVertex("winners", map((Entry<String, Event> e) -> {
+            Event evt = e.getValue();
+            return entry(evt, FIRST_PAST_THE_POST.apply(evt));
+        }));
+
+        final Vertex sink = dag.newVertex("sink", writeMap(MULTIPLE));
+
+        return dag.edge(between(source.localParallelism(1), winners))
+                .edge(between(winners.localParallelism(1), sink));
     }
 
     private void go() throws Exception {
@@ -107,22 +126,11 @@ public class JetSimple {
 
         // Prime a map
         final IMap<String, Event> name2Event = jet.getMap(EVENTS_BY_NAME);
-        try (BufferedReader r = new BufferedReader(new InputStreamReader(JetSimple.class.getResourceAsStream("historical_races.json"), UTF_8))) {
+        try (BufferedReader r = new BufferedReader(new InputStreamReader(JetSimple.class.getResourceAsStream(HISTORICAL), UTF_8))) {
             r.lines().map(l -> JSONSerializable.parse(l, Event::parseBag)).forEach(e -> name2Event.put(e.getName(), e));
         } catch (IOException iox) {
-
+            iox.printStackTrace();
         }
-
-    }
-
-    public static DAG buildDag() {
-        final DAG dag = new DAG();
-
-        Vertex source = dag.newVertex("source", readMap(USER_ID));
-
-        Vertex sink = dag.newVertex("sink", writeMap(WORST_ID));
-
-        return dag.edge(between(source.localParallelism(1), sink));
     }
 
 }
